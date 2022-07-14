0515925eb8d2b577a075dc6b1c0dbf58b776304c
Raj Parmar <rajparmardgtech@gmail.com>
2022-07-14 11:32:30 +0530

    Add new changes 13.07.2022

--- src/app/Portal/Pipes/pipe/pipe.module.ts
+++ src/app/Portal/Pipes/pipe/pipe.module.ts
@@ -5,15 +5,16 @@ import { SelectfilterPipe } from './selectfilter.pipe';
 import { LimittoPipe } from '../limitto.pipe';
 import { MonthtoyearPipe } from '../monthtoyear.pipe';
 import { ArraylengthPipe } from '../arraylength.pipe';
+import { InstypePipe} from '../instype.pipe';
 
 
 
 
 @NgModule({
-  declarations: [MenufilterPipe, SelectfilterPipe,LimittoPipe,MonthtoyearPipe,ArraylengthPipe],
+  declarations: [MenufilterPipe, SelectfilterPipe,LimittoPipe,MonthtoyearPipe,ArraylengthPipe,InstypePipe],
   imports: [
     CommonModule
   ],
-  exports: [MenufilterPipe, SelectfilterPipe,LimittoPipe,MonthtoyearPipe,ArraylengthPipe]
+  exports: [MenufilterPipe, SelectfilterPipe,LimittoPipe,MonthtoyearPipe,ArraylengthPipe,InstypePipe]
 })
 export class PipeModule { }
